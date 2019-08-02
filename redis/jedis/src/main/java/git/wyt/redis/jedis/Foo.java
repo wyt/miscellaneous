d0b797871d5f4faec83f9811bef4e5ca2a795cdb
wangyongtao <wangyt4dev@gmail.com>
2019-08-02 10:42:29 +0800

    增加连接池&集群方式.

--- redis/jedis/src/main/java/git/wyt/redis/jedis/Foo.java
+++ redis/jedis/src/main/java/git/wyt/redis/jedis/Foo.java
@@ -1,39 +1,54 @@
 package git.wyt.redis.jedis;
 
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Foo {
 
-  JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
+  private static final String REDIS_LOCAL_HOST = "localhost";
+  private static final int REDIS_LOCAL_PORT = 6379;
+
+  JedisPool pool = new JedisPool(new JedisPoolConfig(), REDIS_LOCAL_HOST, REDIS_LOCAL_PORT);
 
   public static void main(String[] args) {
     Foo foo = new Foo();
-    foo.usePool();
+    foo.useCluster();
   }
 
+  /** 直接使用单节点Redis */
   public void basic() {
-    Jedis jedis = new Jedis("localhost");
+    Jedis jedis = new Jedis(REDIS_LOCAL_HOST, REDIS_LOCAL_PORT);
     jedis.set("foo", "bar");
     String value = jedis.get("foo");
     System.out.println(value);
-
-    jedis.slaveof("",11);
   }
 
+  /** 对单节点Redis应用连接池 */
   public void usePool() {
     try (Jedis jedis = pool.getResource()) {
-      /// ... do stuff here ... for example
-      jedis.set("foo", "bar");
-      String foobar = jedis.get("foo");
-      jedis.zadd("sose", 0, "car");
-      jedis.zadd("sose", 0, "bike");
-      Set<String> sose = jedis.zrange("sose", 0, -1);
+      jedis.set("foo", "bar001");
+      String value = jedis.get("foo");
+      System.out.println(value);
     }
-    /// ... when closing your application:
     pool.close();
+  }
+
+  /** 使用Redis集群 */
+  public void useCluster() {
+    Set<HostAndPort> jedisClusterNodes = new HashSet<>();
+    // Jedis Cluster will attempt to discover cluster nodes automatically
+    jedisClusterNodes.add(new HostAndPort("192.168.91.146", 7000));
+    jedisClusterNodes.add(new HostAndPort("192.168.91.146", 7001));
+    jedisClusterNodes.add(new HostAndPort("192.168.91.146", 7002));
+    jedisClusterNodes.add(new HostAndPort("192.168.91.146", 7003));
+    jedisClusterNodes.add(new HostAndPort("192.168.91.146", 7004));
+    jedisClusterNodes.add(new HostAndPort("192.168.91.146", 7005));
+
+    JedisCluster jedisCluster = new JedisCluster(jedisClusterNodes);
+    jedisCluster.set("foo", "bar002");
+    String value = jedisCluster.get("foo");
+    System.out.println(value);
   }
 }
