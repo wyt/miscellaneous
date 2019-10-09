1b4121c22b24dd7a26f938d20261b132e6bc6dce
wangyongtao <wangyt4dev@gmail.com>
2019-10-09 16:34:57 +0800

    add springboot2

---
+++ springboot2/src/main/java/git/wyt/springboot2/Springboot2Application.java
@@ -0,0 +1,13 @@
+package git.wyt.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2Application.class, args);
+	}
+
+}
