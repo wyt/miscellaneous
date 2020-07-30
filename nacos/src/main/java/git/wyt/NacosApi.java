0166d15bd13c618fe3fedd37a9dcaec2c5595184
wangyongtao <wangyt4dev@gmail.com>
2020-07-30 19:11:39 +0800

    opt.

--- nacos/src/main/java/git/wyt/NacosApi.java
+++ nacos/src/main/java/git/wyt/NacosApi.java
@@ -2,11 +2,10 @@ package git.wyt;
 
 import com.alibaba.nacos.api.NacosFactory;
 import com.alibaba.nacos.api.PropertyKeyConst;
+import com.alibaba.nacos.api.config.ConfigService;
 import com.alibaba.nacos.api.config.listener.Listener;
 import com.alibaba.nacos.api.exception.NacosException;
-import com.alibaba.nacos.client.config.NacosConfigService;
 
-import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
@@ -17,43 +16,21 @@ import java.util.concurrent.TimeUnit;
  */
 public class NacosApi {
 
-  // -javaagent:E:\idea\projects\yiche\holmes-sniffer\holmes-agent\target\holmes-agent-0.0.1.jar
-  // -Dholmes.sleuth.logstash.url=172.17.28.35:4570
-  // -Dholmes.nacos.server_addr=http://192.168.200.63:8848
-  // -Dholmes.nacos.name_space=94a413cd-1709-4373-8dc5-312c04986b0e
-  // -Dholmes.nacos.data_id=mapi-app-carmodel-sleuth.properties
-  // -Dholmes.nacos.group=DEFAULT_GROUP
-
-  public static void main(String[] args) throws NacosException, IOException, InterruptedException {
+  public static void main(String[] args) throws NacosException, InterruptedException {
 
     String serverAddr = "http://192.168.200.63:8848";
     String nameSpace = "94a413cd-1709-4373-8dc5-312c04986b0e";
     String dataId = "mapi-app-carmodel-sleuth.properties";
     String group = "DEFAULT_GROUP";
 
-    //    String serverAddr = "http://127.0.0.1:8848";
-    //    String nameSpace = "17b74835-9c78-404f-baae-3340a32e6a23";
-    //    String dataId = "mapi-app-carmodel-sleuth.properties";
-    //    String group = "DEFAULT_GROUP";
-
-    //    String serverAddr = "http://127.0.0.1:8848";
-    //    String nameSpace = "";
-    //    String dataId = "mapi-app-carmodel-sleuth.properties";
-    //    String group = "MAPI_APP_GROUP";
-
     Properties properties = new Properties();
 
     properties.put(PropertyKeyConst.SERVER_ADDR, serverAddr);
     if (nameSpace != null) {
       properties.put(PropertyKeyConst.NAMESPACE, nameSpace);
-      System.out.println("name space: " + nameSpace);
     }
 
-    NacosConfigService configService =
-        (NacosConfigService) NacosFactory.createConfigService(properties);
-
-    System.out.println("server status: " + configService.getServerStatus());
-
+    ConfigService configService = NacosFactory.createConfigService(properties);
     configService.addListener(
         dataId,
         group,
@@ -65,13 +42,15 @@ public class NacosApi {
 
           @Override
           public void receiveConfigInfo(String configInfo) {
-            System.out.println("configInfo: " + configInfo);
+            System.out.println("配置变更:");
+            System.out.println(configInfo);
           }
         });
 
     String content = configService.getConfig(dataId, group, 5000);
-    System.out.println("first time receive: " + content);
+    System.out.println("获取配置:");
+    System.out.println(content);
 
-    TimeUnit.SECONDS.sleep(100);
+    TimeUnit.SECONDS.sleep(60 * 10);
   }
 }
